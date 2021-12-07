766f204179f28b83b2a354b62380b80aef7ccf3f
Taranvir Hidkyar <thidkyar@gmail.com>
2021-12-07 10:58:31 -0500

    charscreen complete

--- src/screens/MatchedScreen.js
+++ src/screens/MatchedScreen.js
@@ -1,12 +1,49 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useNavigation, useRoute } from "@react-navigation/core";
+import React from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import tw from "tailwind-rn";
 
-const MatchedScreen = ({loggedInProfile, userSwiped}) => {
+const MatchedScreen = () => {
+    const navigation = useNavigation();
+    const { params } = useRoute();
+    const { loggedInProfile, userSwiped } = params;
+
     return (
-        <View>
-            <Text>MATCHED</Text>
-        </View>
-    )
-}
+        <View style={[tw("h-full bg-red-500 pt-20"), { opacity: 0.89 }]}>
+            <View style={[tw("justify-center px-10 pt-20")]}>
+                <Image
+                    style={tw("h-20 w-full")}
+                    source={{
+                        uri: "https://www.citypng.com/public/uploads/preview/-11595269534beh3mlqure.png",
+                    }}
+                />
+            </View>
 
-export default MatchedScreen
+            <Text style={[tw("text-white text-center mt-5")]}>
+                You and {userSwiped.displayName} have liked each other.
+            </Text>
+            <View style={[tw("flex-row justify-evenly mt-5")]}>
+                <Image
+                    style={[tw("h-32 w-32 rounded-full")]}
+                    source={{ uri: loggedInProfile.photoURL }}
+                />
+                <Image
+                    style={[tw("h-32 w-32 rounded-full")]}
+                    source={{ uri: userSwiped.photoURL }}
+                />
+            </View>
+
+            <TouchableOpacity
+                style={[tw("bg-white m-5 px-10 py-8 rounded-full mt-20")]}
+                onPress={() => {
+                    navigation.goBack();
+                    navigation.navigate("Chats");
+                }}
+            >
+                <Text style={[tw("text-center")]}>Send a message</Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+export default MatchedScreen;
